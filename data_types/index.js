925fc6588fc4a6cccfe1c5a84b84f2de73a5cd44
rashila01 <rashila.mhr123@gmail.com>
2020-09-28 09:26:29 +0545

    initial commit

---
+++ data_types/index.js
@@ -0,0 +1,25 @@
+// Declare variable and assign value to it
+let integer_value=1;
+let float_value=2.89;
+let string_value='hello world';
+let bool_value=false;
+let undefined_value;
+
+// Logging variable value through console.log()
+console.log("=======");
+console.log(integer_value);
+console.log(float_value);
+console.log(string_value);
+console.log(bool_value);
+console.log(undefined_value);
+console.log("======");
+
+// Check data type of variable
+console.log("=======");
+console.log(typeof integer_value);
+console.log(typeof float_value);
+console.log(typeof string_value);
+console.log(typeof bool_value);
+console.log(undefined_value);
+console.log("=======");
+
