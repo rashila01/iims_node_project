f03c086210a418774e38cbe85e1ad4a79dda9708
rashila01 <rashila.mhr123@gmail.com>
2020-09-30 09:25:46 +0545

    Array first class

---
+++ array/index.js
@@ -0,0 +1,19 @@
+// let firstname = 'Rashila', lastname = 'maharjan', address = 'panga', age = '20';
+
+let introduction =['Rashila', 'maharjan', 'panga', '20', 1, true, null, [1,2]];
+//console.log(introduction[2]);
+//introduction[2] = 'kirtipur';
+//console.log(introduction[2]);
+introduction.push(['kathmandu', 'birgunj']);
+introduction = 'pokhara';
+console.log(introduction);
+
+                   // 0         1           2       3
+
+// console.log(firstname, lastname, address, age);
+//console.log(introduction[7]);
+//console.log(introduction[0][1]);
+
+
+//let newArr = [['hello',2,true],['iims','bscit'],['kathmandu','pokhara','dharan']];
+//console.log(newArr[2][2]);
