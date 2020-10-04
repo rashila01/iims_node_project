28fcb1784cb976255475558dc83f97d945b7cf00
rashila01 <rashila.mhr123@gmail.com>
2020-10-04 09:07:18 +0545

    Array and objects

--- array/index.js
+++ array/index.js
@@ -1,12 +1,19 @@
 // let firstname = 'Rashila', lastname = 'maharjan', address = 'panga', age = '20';
 
-let introduction =['Rashila', 'maharjan', 'panga', '20', 1, true, null, [1,2]];
+//let introduction =['Rashila', 'maharjan', 'panga', '20'];
+ //introduction.pop();
+ //introduction.push('pokhara');
+ //console.log(introduction);
+ //introduction.shift();
+ //console.log(introduction);
+ //introduction.unshift('mushroom');
+ //console.log(introduction);
 //console.log(introduction[2]);
 //introduction[2] = 'kirtipur';
 //console.log(introduction[2]);
-introduction.push(['kathmandu', 'birgunj']);
-introduction = 'pokhara';
-console.log(introduction);
+//introduction.push(['kathmandu', 'birgunj']);
+//introduction = 'pokhara';
+//console.log(introduction);
 
                    // 0         1           2       3
 
@@ -17,3 +24,48 @@ console.log(introduction);
 
 //let newArr = [['hello',2,true],['iims','bscit'],['kathmandu','pokhara','dharan']];
 //console.log(newArr[2][2]);
+//introduction.splice(1, 2, 'berry', 'banana',[1,2]);
+// console.log(introduction);
+
+// add joker at index 2 without removing any items
+//add joker at 0 index of 2 index value of dc
+
+// let dc = ['superman', 'batman', 'aquaman'];
+// let villian = ['joker'];
+// let dcHero = ['flash'];
+// console.log(dc.concat(villian,dcHero));
+
+// dc.splice(2,0,'joker');
+// console.log(dc);
+// dc[2].unshift('joker');
+// console.log(dc);
+// dc.splice(3,0,'joker');
+// console.log(dc);
+// console.log(dc.length);
+// delete dc[1];
+// console.log(dc);
+
+// dc.splice(2,1);
+// console.log(dc);
+
+// dc.splice(0,3,'hello', 'world');
+// console.log(dc);
+
+
+// first argument passed on slice will include that element but will remove all elements before it
+// second argument passed on lsice method will not include element of that index and removes every element after that elemetns including itself 
+// let fruits = ["Banana","Orange","Lemon","Apple","Mango"];
+// let citrus=fruits.slice(1,2);
+// console.log(fruits);
+// console.log(citrus);
+
+// let dc = ['superman', 'batman', 'aquaman','flash','wonderwoman'];
+// console.log(dc.slice(3));
+
+let student = {};
+student.firstname='rashila';
+student.lastname='maharjan';
+student.id=191114;
+student.gender='female';
+student.faculty='BIT';
+console.log(student.faculty);
